41bad13a04de9e3714e7fec7e981691bd1f2a86e
stevebagwell <steve_bagwell@sil.org>
2022-11-07 11:10:59 +0000

    rearrange the methods and remove return types

--- src/middleware/RequestExtras.php
+++ src/middleware/RequestExtras.php
@@ -10,9 +10,9 @@ class RequestExtras
     const DEFAULTSKEY = 'defaults';
     const AUTHKEY = 'auth';
 
-    public static function getAddContentTypeFn($contentType): callable
+    public static function getAddContentTypeFn($contentType)
     {
-        return static function (callable $handler) use ($contentType): callable {
+        return static function (callable $handler) use ($contentType) {
             return static function (RequestInterface $request, array $options) use ($handler, $contentType) {
                 $request = $request->withHeader('Content-Type', $contentType);
                 return $handler($request, $options);
@@ -21,34 +21,14 @@ class RequestExtras
     }
 
 
-    private static function countAtLeast(array $input, $minimum){
-        if (! is_array($input)) {
-            return false;
-        }
-        return count($input) >= $minimum;
-    }
-
-    private static function getAuthKeySecret(array $options) {
-        // If there is a specific auth entry, use it
-        if (isset( $options[self::AUTHKEY])) {
-            $auth = $options[self::AUTHKEY];
-            if (! self::countAtLeast($auth, 1)) {
-                throw new \Exception('Middleware client options have an invalid \'auth\' array.', 1666607090);
-            }
-
-            // If there is no specific auth entry, use the default
-        } else {
-            if (!isset($options[self::DEFAULTSKEY][self::AUTHKEY]) ||
-                ! self::countAtLeast($options[self::DEFAULTSKEY][self::AUTHKEY], 1)) {
-                throw new \Exception('Middleware client options have an invalid \'defaults=>auth\' array.', 1666607095);
-            }
-
-            $auth = $options[self::DEFAULTSKEY][self::AUTHKEY];
-        }
-
-        $key = $auth[0];
-        $secret = isset($auth[1]) ? $auth[1] : null;
-        return [$key, $secret];
+    public static function getAddAuthParamsFn($config)
+    {
+        return static function (callable $handler) use ($config) {
+            return static function (RequestInterface $request, array $options) use ($handler, $config) {
+                $request = self::addAuthParams($request, $config);
+                return $handler($request, $options);
+            };
+        };
     }
 
 
@@ -82,14 +62,34 @@ class RequestExtras
         return $request->withUri($uri);
     }
 
+    private static function getAuthKeySecret(array $options) {
+        // If there is a specific auth entry, use it
+        if (isset( $options[self::AUTHKEY])) {
+            $auth = $options[self::AUTHKEY];
+            if (! self::countAtLeast($auth, 1)) {
+                throw new \Exception('Middleware client options have an invalid \'auth\' array.', 1666607090);
+            }
 
-    public static function getAddAuthParamsFn($config): callable
-    {
-        return static function (callable $handler) use ($config) : callable {
-            return static function (RequestInterface $request, array $options) use ($handler, $config) {
-                $request = self::addAuthParams($request, $config);
-                return $handler($request, $options);
-            };
-        };
+            // If there is no specific auth entry, use the default
+        } else {
+            if (!isset($options[self::DEFAULTSKEY][self::AUTHKEY]) ||
+                ! self::countAtLeast($options[self::DEFAULTSKEY][self::AUTHKEY], 1)) {
+                throw new \Exception('Middleware client options have an invalid \'defaults=>auth\' array.', 1666607095);
+            }
+
+            $auth = $options[self::DEFAULTSKEY][self::AUTHKEY];
+        }
+
+        $key = $auth[0];
+        $secret = isset($auth[1]) ? $auth[1] : null;
+        return [$key, $secret];
+    }
+
+
+    private static function countAtLeast(array $input, $minimum){
+        if (! is_array($input)) {
+            return false;
+        }
+        return count($input) >= $minimum;
     }
 }
